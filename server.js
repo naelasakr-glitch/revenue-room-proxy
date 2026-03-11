300d9376fa3b74c790877fed4635c24ce4d2075d
naelasakr-glitch <naelasakr@barkb2b.com>
2026-03-11 19:37:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
+app.options("*", cors({ origin: "*" }));
 app.use(express.json({ limit: "2mb" }));
 
 app.post("/chat", async (req, res) => {
